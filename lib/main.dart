f3cd9bb31e9bfd293bb6e573f0c3a3df06e33253
Giang <giangpro1935@gmail.com>
2026-04-27 18:36:00 +0700

    optimize firebase loading

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-
+////
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
